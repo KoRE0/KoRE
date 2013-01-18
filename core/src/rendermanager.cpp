0f736ae41f75fed09c65a9c5d98c8ba5582e524f
Dominik Lazarek <dominik.lazarek@gmail.com>
2013-01-18 14:20:37 +0100

    Worked on the operation system

--- core/src/rendermanager.cpp
+++ core/src/rendermanager.cpp
@@ -33,56 +33,6 @@ kore::RenderManager::RenderManager(void) {
 kore::RenderManager::~RenderManager(void) {
 }
 
-void kore::RenderManager::renderMesh
-(const std::shared_ptr<Mesh>& mesh,
-const std::shared_ptr<Shader>& shader,
-const std::shared_ptr<Camera>& camera) {
-    const std::vector<kore::ShaderInput>& vAttributes = shader->getAttributes();
-    for (unsigned int i = 0; i < vAttributes.size(); ++i) {
-        const kore::ShaderInput& shaderAtt = vAttributes[i];
-        const kore::MeshAttributeArray* meshAtt =
-            mesh->getAttributeByName(shaderAtt.name);
-
-        if (!meshAtt) {
-            Log::getInstance()->write("[ERROR] Mesh %s does not have an"
-                                      "Attribute %s",
-                                      mesh->getName().c_str(),
-                                      shaderAtt.name.c_str());
-            return;
-        }
-
-        glEnableVertexAttribArray(shaderAtt.location);
-        glVertexAttribPointer(shaderAtt.location, meshAtt->numComponents,
-                              meshAtt->componentType, GL_FALSE,
-                              0, meshAtt->data);
-    }
-
-    shader->applyShader();
-    // Update uniforms
-    GLint iView =
-        glGetUniformLocation(shader->getProgramLocation(), "view");
-
-    GLint iProj =
-        glGetUniformLocation(shader->getProgramLocation(), "projection");
-
-    GLint iModel =
-        glGetUniformLocation(shader->getProgramLocation(), "model");
-
-    glUniformMatrix4fv(iView, 1, GL_FALSE, glm::value_ptr(camera->getView()));
-
-    glUniformMatrix4fv(iProj, 1, GL_FALSE,
-                        glm::value_ptr(camera->getProjection()));
-
-    glUniformMatrix4fv(iModel, 1, GL_FALSE, glm::value_ptr(glm::mat4(1.0f)));
-
-if (mesh->hasIndices()) {
-   glDrawElements(mesh->getPrimitiveType(), mesh->getIndices().size(),
-       GL_UNSIGNED_INT, &mesh->getIndices()[0]);
-} else {
-     glDrawArrays(mesh->getPrimitiveType(), 0, mesh->getNumVertices());
- }
-}
-
 const glm::ivec2& kore::RenderManager::getRenderResolution() const {
     return _renderResolution;
 }
