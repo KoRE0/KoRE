45e0b5d33fbeafbee092e54c8e7cf62b5feb0fd8
Andreas Weinmann <andy.weinmann@gmail.com>
2013-02-05 17:14:53 +0100

    update

--- demos/MainQT5/glwindow.h
+++ demos/MainQT5/glwindow.h
@@ -1,5 +1,28 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+/*
+  Copyright © 2012 The KoRE Project
+
+  This file is part of KoRE.
+
+  KoRE is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+  KoRE is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with KoRE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/************************************************************************/
+/* \author Andreas Weinmann                                             */
+/************************************************************************/
+
+#ifndef GLWINDOW_H
+#define GLWINDOW_H
 
 #include <QWindow>
 
@@ -26,4 +49,4 @@ private:
     QOpenGLContext* _context;
 
 };
-#endif // MAINWINDOW_H
+#endif // GLWINDOW_H
